cc772485f3051b0531229baacad3cf2e7f56f0e5
oy <oy@192.168.40.65>
2018-12-20 14:05:55 +0800

    11

--- src/com/wwwxy/employeemanagement/control/CheckDetailsControl.java
+++ src/com/wwwxy/employeemanagement/control/CheckDetailsControl.java
@@ -1,9 +1,5 @@
 package com.wwwxy.employeemanagement.control;
-import java.sql.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.Date;
 import com.wwwxy.employeemanagement.dao.CheckDetailsDao;
 import com.wwwxy.employeemanagement.entity.CheckDetails;
 public class CheckDetailsControl {
@@ -12,31 +8,20 @@ public class CheckDetailsControl {
 	public List<CheckDetails> getAllCheckDetails(){
 		return cdd.getAllCheckDetails();
 	}
-	
-	
 	//输入员工ID查询考勤信息
 		public List<CheckDetails> getCheckDetailsByempid(int empid){
 			return cdd.getCheckDetailsByempid(empid);
 		}
-		
-		
-		
 		//新增考勤信息
 	public int addCheckDetails(CheckDetails cd){
 		return cdd.addCheckDetails(cd);
 	}
-	
-	
 	//删除考勤信息
 	public int delCheckDetailsBYCid(int cid){
 		return cdd.delCheckDetailsBYCid(cid);
 	}
-	
-	
-	
 	//修改考勤信息
 	public int CheckDetailsByecid(CheckDetails cd){
 		return cdd.updateCheckDetailsBYCid(cd);
 	}
-	
 }
